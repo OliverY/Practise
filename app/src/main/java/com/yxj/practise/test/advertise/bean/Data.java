d91ca2d266870c46020905de4aac2c2b426621a9
yanxujun <yanxujun@yuantutech.com>
2018-07-30 16:26:16 +0800

    广告view BaseAdView

---
+++ app/src/main/java/com/yxj/practise/test/advertise/bean/Data.java
@@ -0,0 +1,20 @@
+package com.yxj.practise.test.advertise.bean;
+
+/**
+ * Author:  Yxj
+ * Time:    2018/7/30 下午4:13
+ * -----------------------------------------
+ * Description:
+ */
+public class Data {
+
+    public int resourceId;
+    public String tips;
+
+    public Data(int resourceId, String tips) {
+        this.resourceId = resourceId;
+        this.tips = tips;
+    }
+
+
+}
