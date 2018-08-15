7f6a2c3ee1f197cf532e40a116c422a7254cc98a
yanxujun <yanxujun@yuantutech.com>
2018-08-15 08:48:45 +0800

    add 流式布局

--- app/src/main/java/com/yxj/practise/test/flow/FlowActivity.java
+++ app/src/main/java/com/yxj/practise/test/flow/FlowActivity.java
@@ -1,5 +1,6 @@
 package com.yxj.practise.test.flow;
 
+import android.graphics.BitmapRegionDecoder;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
